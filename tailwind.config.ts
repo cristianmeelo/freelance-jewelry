661fdc50c1c2963ae6aa47c84b3e561eb3e571b7
cristian_melo <cristian_melo@sicredi.com.br>
2024-12-15 19:23:24 -0300

    started project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,16 @@
 import type { Config } from "tailwindcss";
 
+import { skeleton, contentPath } from "@skeletonlabs/skeleton/plugin";
+import * as themes from "@skeletonlabs/skeleton/themes";
+
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}", contentPath(import.meta.url, "react")],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    skeleton({
+      themes: [themes.crimson],
+    }),
+  ],
 } satisfies Config;
